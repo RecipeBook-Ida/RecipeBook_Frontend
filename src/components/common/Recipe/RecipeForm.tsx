0fec98001b652b7e6ef40305a6ec546c8039a7cd
idaplovas <141772242+idaplovas@users.noreply.github.com>
2024-05-02 10:36:55 +0200

    start dynamically add subRecipeFroms

--- src/components/common/Recipe/RecipeForm.tsx
+++ src/components/common/Recipe/RecipeForm.tsx
@@ -1,19 +1,30 @@
 import { Box, Button, MenuItem, TextField } from "@mui/material";
 import { User, dummyUser } from "../../../types/UserType";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SubRecipeForm from "./SubRecipeForm";
 import { usePostRecipe } from "../../../services/recipe/postRecipe";
 import { RecipePost } from "../../../types/RecipeType";
+import { useGetAllIngredients } from "../../../services/ingredient/getIngredient";
+import { Ingredient } from "../../../types/Ingredient";
 
 interface RecipeFormProps {}
 
 const RecipeForm: React.FC<RecipeFormProps> = ({}) => {
+  const ingredientHook = useGetAllIngredients();
+  const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+
+  useEffect(() => {
+    if (!ingredientHook.isLoading && ingredientHook.data) {
+      setIngredients([...(ingredientHook.data as Ingredient[])]);
+    }
+  }, [ingredientHook.data, ingredientHook.isLoading, ingredientHook.isStale]);
+
   const type = ["Frokost", "Lunsj", "Middag", "Dessert", "Annet"];
   const cuisine = ["Italiensk", "Asiatisk", "Vegansk", "Vegetariansk", "Annet"];
 
   const user: User = dummyUser;
   const postRecipe = usePostRecipe();
-  const [formData, setFormData] = useState<RecipePost>({
+  const [formData, setFormData] = useState({
     title: "",
     description: "",
     cooktime: 0,
@@ -21,7 +32,19 @@ const RecipeForm: React.FC<RecipeFormProps> = ({}) => {
     cuisine: "",
     type: "",
     portion: 0,
-    subRecipeIds: [],
+    subRecipes: [
+      {
+        title: "",
+        instructions: "",
+        ingredients: [
+          {
+            quantity: 0,
+            unit: "",
+            ingredientId: 0,
+          },
+        ],
+      },
+    ],
     appUser: user.id,
   });
   const [formErrors, setFormErrors] = useState({
@@ -32,7 +55,7 @@ const RecipeForm: React.FC<RecipeFormProps> = ({}) => {
     cuisine: false,
     type: false,
     portion: false,
-    subRecipeIds: false,
+    subRecipes: false,
     appUser: false,
   });
 
@@ -60,9 +83,55 @@ const RecipeForm: React.FC<RecipeFormProps> = ({}) => {
     e.preventDefault();
     if (e.target.checkValidity()) {
       alert("Oppskrift laget!");
-      await postRecipe.mutateAsync(formData);
+      //const ids = formData.subRecipes.map((subRecipe) => subRecipe.title);
+      const createRecipe: RecipePost = {
+        title: formData.title,
+        description: formData.description,
+        cooktime: formData.cooktime,
+        image: formData.image,
+        cuisine: formData.cuisine,
+        type: formData.type,
+        portion: formData.portion,
+        subRecipeIds: [1, 2],
+        appUser: user.id,
+      };
+      await postRecipe.mutateAsync(createRecipe);
     } else alert("Venligst fyll ut alle felt.");
   };
+
+  const handleAddSubRecipe = async (e: any) => {
+    setFormData({
+      ...formData,
+      subRecipes: [
+        ...formData.subRecipes,
+        {
+          title: "",
+          instructions: "",
+          ingredients: [
+            {
+              quantity: 0,
+              unit: "",
+              ingredientId: 0,
+            },
+          ],
+        },
+      ],
+    });
+
+    console.log(formData);
+  };
+
+  const handleDelete = (i: number) => {
+    console.log(i);
+    const deleteSubRecipe = [...formData.subRecipes];
+    deleteSubRecipe.splice(i, 1);
+    setFormData({ ...formData, subRecipes: deleteSubRecipe });
+  };
+
+  if (ingredientHook.isPending || !ingredients) return "Loading...";
+
+  if (ingredientHook.error)
+    return "An error has occurred: " + ingredientHook.error.message;
 
   return (
     <Box
@@ -138,11 +207,26 @@ const RecipeForm: React.FC<RecipeFormProps> = ({}) => {
         ))}
       </TextField>
 
-      <SubRecipeForm></SubRecipeForm>
+      {formData.subRecipes.map((subRecipe, index) => (
+        <>
+          <SubRecipeForm
+            /*     formData={formData}
+            setFormData={setFormData} */
+            ingredients={ingredients}
+          ></SubRecipeForm>
+          <Button onClick={() => handleDelete(index)}>Delete</Button>
+        </>
+      ))}
+
+      <Button variant="contained" color="primary" onClick={handleAddSubRecipe}>
+        + subrecipe
+      </Button>
 
       <Button variant="contained" color="primary" type="submit">
         Submit
       </Button>
+
+      <p>{JSON.stringify(formData)}</p>
     </Box>
   );
 };
