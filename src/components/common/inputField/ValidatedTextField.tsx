47a5cda31064a9f50c4e89d1f900e3fba2e9b1ce
idaplovas <141772242+idaplovas@users.noreply.github.com>
2024-05-22 09:36:09 +0200

    error nessaage on submit

--- src/components/common/inputField/ValidatedTextField.tsx
+++ src/components/common/inputField/ValidatedTextField.tsx
@@ -1,35 +1,53 @@
 import { TextField } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface ValidatedTextFieldProps {
   label: string;
   validator: any;
-  onChange: any;
+  onChange: (newValue: any, isValid: boolean) => void;
+  number?: true;
+  props?: any;
+  submit?: boolean;
 }
 
 const ValidatedTextField: React.FC<ValidatedTextFieldProps> = ({
   label,
   validator,
   onChange,
+  number,
+  props,
+  submit,
 }) => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState(number ? 0 : "");
   const [error, setError] = useState(false);
 
+  useEffect(() => {
+    if (!value && submit) {
+      const errorMessage = validator(value);
+      setError(errorMessage);
+      onChange(value, !errorMessage);
+    }
+  }, [submit]);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = e.target.value;
+    const newValue = number ? +e.target.value : e.target.value;
     const errorMessage = validator(newValue);
     setValue(newValue);
     setError(errorMessage);
-    onChange(!errorMessage);
+    onChange(newValue, !errorMessage);
   };
 
   return (
     <TextField
+      {...props}
       label={label}
       value={value}
       onChange={handleChange}
       error={!!error}
       helperText={error}
+      variant="outlined"
+      required
+      type={number && "number"}
     />
   );
 };
